2280bc539b32ce83fdaf24df61ebe4748183ddd3
Artem Archenkov <r203mail@gmail.com>
2020-03-21 10:08:57 +0400

    added js script for slider

--- work/work3/scripts/scripts.js
+++ work/work3/scripts/scripts.js
@@ -1,6 +1,6 @@
 const show = document.querySelector("#hamburger-link");
 const menu = document.querySelector("#hamburger-menu");
-const close1 = document.querySelector("#hamburger-menu__close");
+const closebtn = document.querySelector("#hamburger-menu__close");
 let links = document.querySelectorAll('.nav__link');
 
 function toggleMenu() {
@@ -9,7 +9,7 @@ function toggleMenu() {
 };
 
 show.addEventListener("click", toggleMenu);
-close1.addEventListener("click", toggleMenu);
+closebtn.addEventListener("click", toggleMenu);
 links.forEach(function(element){
     element.addEventListener('click' , toggleMenu);
 });
